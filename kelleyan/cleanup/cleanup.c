e849fbf847fbbf7a5b7f33151a3d0678ebb8c7fd
southpaw27 <awkelley27@gmail.com>
2014-02-13 13:50:41 -0600

    Now using LINE_MAX and a switch statement. Also added support for ^G character

--- kelleyan/cleanup/cleanup.c
+++ kelleyan/cleanup/cleanup.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <limits.h>
 
 /*
 This program removes extra crap that script puts into its output.
@@ -7,13 +8,17 @@ This particular implementation might not remove all the extra
 things script puts in. Colin Trembley was a great person to chat
 with while writing this, and his hex numbers for the characters
 came in handy. This could definitely use some more improvement.
+
+Authors:
+Andrew Kelley
+Mark Lewis
 */
 
 int main(int argc, char* argv[]) {
 	FILE *fp;
-	char str[80];
+	char str[LINE_MAX];
 	int i, j;
-	char strClean[80];
+	char strClean[LINE_MAX];
 	// open file
 	fp = fopen(argv[1], "r");
 	// check for null file
@@ -27,28 +32,35 @@ int main(int argc, char* argv[]) {
 		printf("Failed to open write file\n");
 		return 1;
 	} // if
-
-	while(fgets(str, 80, fp) != NULL) {
+//0x07 is ^G
+	while(fgets(str, LINE_MAX, fp) != NULL) {
 		j = 0;
-		for(i = 0; i < 80; i++) {
-			if(str[i] == '\r') {
-				i++;
-				strClean[j++] = '\n';
-			} else if(str[i] == 0x08) {
-				// skip ahead for i because ^H followed by ^[[K
-				i += 3;
-				// go back one character for each backspace
-				j--;
-			} else if(str[i] == 0x1B) {
-				// case of beginning ^[
-				continue;
-			} else {
-				strClean[j++] = str[i];
-			} // if else
+		for(i = 0; str[i] != '\0'; i++) {
+			switch (str[i]) {
+				case '\r' :
+					i++;
+					strClean[j++] = '\n';
+					break;
+				case 0x08 : 
+					// skip ahead for i because ^H followed by ^[[K
+					i += 3;	
+					// go back one character for each backspace
+					j--;
+					break;
+				case 0x1B :
+					// case of beginning ^[
+				case 0x07 :
+					// case of ^G
+					break;
+				default :
+					strClean[j++] = str[i];
+					break;	
+			} // switch
 		} // for
 		strClean[j] = '\0';
 		fputs(strClean, fileWrite);
 	} // while
+	// close files
 	fclose(fp);
 	fclose(fileWrite);
 
